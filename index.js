eda39c79b71ea22bf1266ce1628828faff12f0f7
dark sorceror <haoyan160@gmail.com>
2024-05-08 20:36:06 -0600

    v1.0.2 - Fixed bugs

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+/*
+PLANS
+- add a countdown mode
+- add a confirm password at the end
+- 
+*/
+
+const VERSION = "1.0.2";
+
 var solved = false;
 var inputs = [];
 var currentRequirement = 1;
+
+function toggleLDMode() {
+    var DMBtn = document.getElementById('dm-btn');
+    var DMBtnMoon = document.getElementById('dm-btn-moon');
+    var DMBtnSun = document.getElementById('dm-btn-sun');
+
+    DMBtn.onclick = function() {
+        BODY.classList.toggle('dm')
+        DMBtnMoon.classList.toggle('a');
+        DMBtnSun.classList.toggle('a');
+    };
+}
 
 function timer(pace = 1000) {
     var h = 0, m = 0, s = 0;
@@ -35,6 +56,7 @@ const log = {
 
 function updateRequirements(string) {
     var boolList = [];
+    var right = document.getElementById('right');
 
     for (i = 1; i <= currentRequirement; i++) {
         var req = document.getElementById(i);
@@ -42,7 +64,7 @@ function updateRequirements(string) {
         req.children[2].innerText = `Password must contain '${log[i]}'`;
         
         if (string.includes(Object.values(log)[i - 1])) {
-            req.classList.add('check');
+            req.classList.add('active', 'check');
             boolList.push(true);
         } else {
             req.classList.remove('check');
@@ -53,18 +75,22 @@ function updateRequirements(string) {
             if ((currentRequirement + 1) < (Object.keys(log).length + 1)) {
                 currentRequirement += 1;
                 addRequirement();
-                document.getElementById('right').style.transform = `translate(0%, ${((currentRequirement - 1) * 55)}px)`;
+                right.style.transform = `translate(0%, ${((currentRequirement - 1) * 55)}px)`;
             } else {
                 console.log("game over");
                 break;
+                // WIP
             }
         }
     }
 }
 
 function addRequirement() {
-    if (document.getElementById(currentRequirement) == null) {
-        document.getElementById('right').insertAdjacentHTML('afterBegin', `
+    var currentReq = document.getElementById(currentRequirement);
+    var right = document.getElementById('right');
+
+    if (currentReq == null) {
+        right.insertAdjacentHTML('afterBegin', `
             <div class="requirement" id="${currentRequirement}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="check"><path d="M20 6 9 17l-5-5"/></svg>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
@@ -77,33 +103,28 @@ function addRequirement() {
 window.onload = function () {
     BODY = document.body;
 
-    var DMBtn = document.getElementById('dm-btn');
-    var DMBtnMoon = document.getElementById('dm-btn-moon');
-    var DMBtnSun = document.getElementById('dm-btn-sun');
     var pwdInput = document.getElementById('password');
 
-    var pasin = document.getElementById('left');
-    var requirements = document.getElementById('right');
+    var left = document.getElementById('left');
+    var right = document.getElementById('right');
 
     timer(1000);
 
-    DMBtn.onclick = function() {
-        BODY.classList.toggle('dm')
-        DMBtnMoon.classList.toggle('a');
-        DMBtnSun.classList.toggle('a');
-    };
+    toggleLDMode();
 
     pwdInput.oninput = function(x) {
         //x["data"] ? inputs.push(x["data"]) : inputs.pop();
 
-        pasin.classList.add('active');
-        requirements.classList.add('active');
+        left.classList.add('active');
+        right.classList.add('active');
 
-        if (currentRequirement == 1) addRequirement(); 
+        if (currentRequirement == 1) addRequirement();
 
         updateRequirements(pwdInput.value);
 
-        document.getElementById(currentRequirement).classList.add('active');
+        var currentReq = document.getElementById(currentRequirement);
+
+        currentReq.classList.add('active');
     }
 
 };
